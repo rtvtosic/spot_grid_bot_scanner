9e6c4dd5cbf67318b6d713b00240050d71043e94
cactus3106@gmail.com <anton3105@bk.ru>
2026-02-25 00:46:17 +0300

    добавлен запрос на получение обьема за 24ч

--- bot.py
+++ bot.py
@@ -13,25 +13,30 @@ class Bot:
     def set_exchange(self, new_exchange_name: str):
         self.exchange_name = new_exchange_name
 
-    # загрузка спотовых пар к USDT с биржи
-    def get_market_data(self) -> list[dict]:
-        if self.exchange_name == 'bybit':
-            exchange = ccxt.bybit()
+    # поиск подходящих активов
+    def get_suitable_symbols(self):
+        # if self.exchange_name == 'bybit':
+        #     exchange = ccxt.bybit()
         
         # запрос данных с биржи
         markets = self.exchange.fetch_markets()
-
         # спотовые пары к USDT
         symbols = [m['symbol'] for m in markets if m['spot'] and m['quote'] == 'USDT']
         
         # загрузка свечей опр. пары
         for symbol in symbols:
-            ohlcv = self.exchange.fetch_ohlcv(symbol)
+            ohlcv = self.exchange.fetch_ohlcv(symbol=symbol,
+                                              timeframe='1m',
+                                              limit=100)
             df = pd.DataFrame(data=ohlcv,
                               columns=['timestamp', 'open', 'high',
                                         'low', 'close', 'volume'])
             
-
+            # проверка обьема за 24ч
+            ticker = self.exchange.fetch_ticker(symbol)
+            volume_24h = ticker['quoteVolume']
+            
+            
 
 
             print(df.head())
@@ -45,6 +50,6 @@ if __name__ == "__main__":
 
     print(bot.get_exchange())
     
-    print(bot.get_market_data())
+    print(bot.get_suitable_symbols())
 
 
